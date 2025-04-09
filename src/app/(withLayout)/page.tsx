9d5a862514fd7d69bce870b1cadb84d1da42a152
prouzzal <prouzzal297@gmail.com>
2025-04-09 09:39:39 +0600

    header slider added

--- src/app/(withLayout)/page.tsx
+++ src/app/(withLayout)/page.tsx
@@ -5,6 +5,8 @@ import Slider from "@/components/Slider/Slider";
 const page = () => {
   return (
     <div className="">
+      <Slider />
+
       <h1 className="text-center text-2xl">This is Home Page</h1>
     </div>
   );
