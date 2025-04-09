ec992cb3e4a5fc530c1f4fb2ebae7eca3aa5eebc
prouzzal <prouzzal297@gmail.com>
2025-04-09 09:40:11 +0600

    people category and new arrival added

--- src/app/(withLayout)/page.tsx
+++ src/app/(withLayout)/page.tsx
@@ -6,7 +6,10 @@ const page = () => {
   return (
     <div className="">
       <Slider />
-
+      <PeopleCategory />
+      <div className="mx-4 md:mx-10">
+        <NewArrival />
+      </div>
       <h1 className="text-center text-2xl">This is Home Page</h1>
     </div>
   );
